a198b857253db6e2a5095817ba5585474b25ad45
Ludo <ludovic.pouet@gmail.com>
2025-05-19 15:00:31 +0200

    FizzBuzz_end

--- Kata#6/FizzBuzz_ludo.py
+++ Kata#6/FizzBuzz_ludo.py
@@ -2,20 +2,20 @@
 
 
 
-
 def fizzBuzz(n):
     answer = []
-    answer.extend([""] * n)
-    for i in range(1, n):
+
+    for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            answer[i] = "FizzBuzz"
+            answer.append("FizzBuzz")
         elif i % 3 == 0:
-            answer[i] = "Fizz"
+            answer.append("Fizz")
         elif i % 5 == 0:
-            answer[i] = "Buzz"
+            answer.append("Buzz")
         else:
-            answer[i] = str(i)
-    print(answer)
+            answer.append(str(i))
+
+    return answer
 
 print(fizzBuzz(3))
 print(fizzBuzz(5))
